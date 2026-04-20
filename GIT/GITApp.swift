2513170ed2418385d046bfe8520d2ce3804f1bab
michellel314 <michelle.lee.087@gmail.com>
2026-04-20 17:34:04 -0400

    Initial Commit

---
+++ GIT/GITApp.swift
@@ -0,0 +1,17 @@
+//
+//  GITApp.swift
+//  GIT
+//
+//  Created by Michelle Lee on 4/20/26.
+//
+
+import SwiftUI
+
+@main
+struct GITApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
