3ab5f4813c0f01cee65795bcd0418c35813896ee
michellel314 <michelle.lee.087@gmail.com>
2026-04-20 17:57:15 -0400

    added the SampleView file

---
+++ GIT/SampleView.swift
@@ -0,0 +1,18 @@
+//
+//  SampleView.swift
+//  GIT
+//
+//  Created by Michelle Lee on 4/20/26.
+//
+
+import SwiftUI
+
+struct SampleView: View {
+    var body: some View {
+        Text("Look at this sample view")
+    }
+}
+
+#Preview {
+    SampleView()
+}
