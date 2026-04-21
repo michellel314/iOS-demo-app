92ae6a9be0c7d99a3c9b8fa7ea3b6e426b7ec6f0
michellel314 <michelle.lee.087@gmail.com>
2026-04-20 20:22:01 -0400

    Added a file named SecondView

---
+++ GIT/SecondView.swift
@@ -0,0 +1,18 @@
+//
+//  SecondView.swift
+//  GIT
+//
+//  Created by Michelle Lee on 4/20/26.
+//
+
+import SwiftUI
+
+struct SecondView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SecondView()
+}
