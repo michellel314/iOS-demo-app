11228387ff3061b1c1f3cd643f012e2cec3c462a
michellel314 <michelle.lee.087@gmail.com>
2026-04-22 22:20:36 -0400

    Modified SecondView to have an eye

--- GIT/SecondView.swift
+++ GIT/SecondView.swift
@@ -9,7 +9,17 @@ import SwiftUI
 
 struct SecondView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack{
+            Circle()
+                .stroke(Color.black, lineWidth: 4)
+                .foregroundStyle(.white)
+                .frame(width: 200, height: 200)
+            Circle()
+                .foregroundStyle(.black)
+                .frame(width: 100, height: 100)
+               
+                
+        }
     }
 }
 
