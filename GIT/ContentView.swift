84bac10db1fb632cb32579e91eb437d16d14c8bc
michellel314 <michelle.lee.087@gmail.com>
2026-04-22 22:17:02 -0400

    Modified ContentView to be holder for all the tabs for the pages

--- GIT/ContentView.swift
+++ GIT/ContentView.swift
@@ -9,7 +9,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Placeholder, this will be the front/first page")
+        TabView{
+            Tab("Hi", systemImage: "person.circle"){
+                Feature()
+            }
+            Tab("It's", systemImage: "rectangle.and.pencil.and.ellipsis"){
+                SecondView()
+            }
+        }
     }
 }
 
